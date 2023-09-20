0b11a8232c6d732a5ce38ae1b77b19c87536ceaa
zhuhw <zhwren0211@whu.edu.cn>
2023-09-20 17:46:14 +0800

    change filelist dir

--- Jinja/templates/cfg/tb.f
+++ Jinja/templates/cfg/tb.f
@@ -8,10 +8,6 @@
 {{f}}
 {% endfor %}
 
-{% for agent in cfg.agents %}
--F ../agent/{{agent.name}}/{{agent.name}}_flist.f
-{% endfor %}
-
 -F ../env/{{cfg.proj}}_{{cfg.module}}_flist.f
 ../th/harness.sv
 -F ../tc/tc.list
--- Jinja/templates/env/flist.f
+++ Jinja/templates/env/flist.f
@@ -1,2 +1,7 @@
 +incdir+.
+
+{% for agent in cfg.agents %}
+-F ../agent/{{agent.name}}/{{agent.name}}_flist.f
+{% endfor %}
+
 {{cfg.proj}}_{{cfg.module}}_pkg.sv
